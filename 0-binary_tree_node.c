056ed51dade6231d69fcd9253c13348d1c20deaa
EngineerLucky <lucksehlatshwayo19@gmail.com>
2024-04-11 15:33:41 -0700

    1-binary_tree_insert_left.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,6 +8,7 @@
 *
 * Return: NULL if there is an error, otherwise a pointer to the new node.
 */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
@@ -24,3 +25,4 @@ new_node->right = NULL;
 
 return (new_node);
 }
+
